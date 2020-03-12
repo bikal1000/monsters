f76855b40e8d0ad33759e46b4b1d7418272a0842
bikal1000 <bikal1000.bs@gmail.com>
2020-03-12 22:55:41 +0545

    Updates

---
+++ precache-manifest.667c7f35e1df1ed7701dc57c8c207f68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9d8165a39616919ce69540c9405d255",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "1841fc782fc056a7fac4",
+    "url": "/monsters/static/css/main.be205806.chunk.css"
+  },
+  {
+    "revision": "ad89debeae4b576d39fd",
+    "url": "/monsters/static/js/2.7bd990ac.chunk.js"
+  },
+  {
+    "revision": "1841fc782fc056a7fac4",
+    "url": "/monsters/static/js/main.87654bf7.chunk.js"
+  },
+  {
+    "revision": "343c9f875db61e2da038",
+    "url": "/monsters/static/js/runtime-main.c437149d.js"
+  }
+]);
